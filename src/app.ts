4c599b614110dc1c8f3eb97c173709659cd44019
SudeepPirangi <sudeep_edu@yahoo.com>
2022-07-14 09:09:13 +0530

    Adds CORS to the app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 import router from "./routes/image-uploads.routes";
 
 const app = express();
 
 app.use("/uploads", express.static("uploads"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(router);
 
 export default app;
